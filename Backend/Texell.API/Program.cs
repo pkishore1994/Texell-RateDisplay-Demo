2f3a5219f59db97beb99f944b9000e54864b0110
Asif Hameed <73539971+asifhameed8@users.noreply.github.com>
2025-05-13 12:51:08 +0500

    Create Program.cs

---
+++ Backend/Texell.API/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.Run();
