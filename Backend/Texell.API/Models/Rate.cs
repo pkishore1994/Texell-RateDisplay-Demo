f32ef29749e4851b8d8f43af2c08bbe782452688
Asif Hameed <73539971+asifhameed8@users.noreply.github.com>
2025-05-13 12:54:34 +0500

    Create Rate.cs

---
+++ Backend/Texell.API/Models/Rate.cs
@@ -0,0 +1,9 @@
+namespace Texell.API.Models
+{
+    public class Rate
+    {
+        public string Type { get; set; }
+        public string RateValue { get; set; }
+        public string Term { get; set; }
+    }
+}
